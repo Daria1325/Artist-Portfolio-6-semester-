0f677a2fddd35a0323a83a87f55c838d4c067286
Daria Bozhenko <ravena1893@gmail.com>
2022-01-29 16:33:48 +0200

    smth

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,1 +1,42 @@
-package cmd
+package main
+
+import (
+	"fmt"
+	"github.com/daria/Portfolio/backend/cmd/client"
+	"github.com/daria/Portfolio/backend/cmd/database"
+	"github.com/daria/Portfolio/backend/cmd/server"
+	cnfg "github.com/daria/Portfolio/backend/config"
+)
+
+var (
+	ConfigPath = "configs/dataConfig.toml"
+)
+
+func configService() (*cnfg.Config, error) {
+	config, err := cnfg.NewConfigPath(configPath)
+	if err != nil {
+		return nil, err
+	}
+	err = client.Connect.InitConn(config.BindAddrHost, config.BindAddrServer, config.FileSize)
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
+}
+func main() {
+	config, err := cnfg.NewConfigPath(ConfigPath)
+	if err != nil {
+		_ = fmt.Errorf("%s", err.Error())
+		return
+	}
+	MainServer := server.Server{}
+	MainServer.Repo = database.Init(config)
+	defer MainServer.Repo.Close()
+
+	err = client.Start(config)
+	if err != nil {
+		_ = fmt.Errorf("%s", err.Error())
+		return
+	}
+	defer client.Connect.Conn.Close()
+}
---
+++ backend/config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import "github.com/BurntSushi/toml"
+
+type Config struct {
+	BindAddr   string `toml:"bind_addr"`
+	DbUser     string `toml:"db_user"`
+	DbName     string `toml:"db_name"`
+	DbPassword string `toml:"db_password"`
+	DbPort     string `toml:"db_port"`
+	DbHost     string `toml:"db_host"`
+}
+
+func NewConfig() *Config {
+	return &Config{
+		BindAddr: ":3000",
+	}
+}
+func NewConfigPath(configPath string) (*Config, error) {
+	config := NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		return config, err
+	}
+	return config, nil
+}
