e2313279d523e6e9d99c2ccd6bb06a081a4d5b6e
Daria Bozhenko <ravena1893@gmail.com>
2022-01-30 17:37:15 +0200

    front

---
+++ backend/cmd/client/client.go
@@ -0,0 +1,51 @@
+package client
+
+import (
+	"fmt"
+	cnfg "github.com/daria/Portfolio/backend/config"
+	"github.com/gorilla/mux"
+	"html/template"
+	"net/http"
+)
+
+func about(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("frontend/html/about.html", "frontend/html/header.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	t.ExecuteTemplate(w, "about", nil)
+}
+func clients(w http.ResponseWriter, r *http.Request) {
+
+}
+func work(w http.ResponseWriter, r *http.Request) {
+
+}
+func contact(w http.ResponseWriter, r *http.Request) {
+
+}
+func show_series(w http.ResponseWriter, r *http.Request) {
+
+}
+func show_picture(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Start(config *cnfg.Config) error {
+	rtr := mux.NewRouter()
+
+	rtr.HandleFunc("/about", about).Methods("GET")
+	rtr.HandleFunc("/clients", clients).Methods("GET")
+	rtr.HandleFunc("/series", work).Methods("GET")
+	rtr.HandleFunc("/contact", contact).Methods("GET")
+	rtr.HandleFunc("/series/{id:[0-9]+}", show_series).Methods("GET")
+	rtr.HandleFunc("/series/{id:[0-9]+}/{id:[0-9]+}", show_picture).Methods("GET")
+
+	err := http.ListenAndServe(config.BindAddr, rtr)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -12,17 +12,6 @@ var (
 	ConfigPath = "configs/dataConfig.toml"
 )
 
-func configService() (*cnfg.Config, error) {
-	config, err := cnfg.NewConfigPath(configPath)
-	if err != nil {
-		return nil, err
-	}
-	err = client.Connect.InitConn(config.BindAddrHost, config.BindAddrServer, config.FileSize)
-	if err != nil {
-		return nil, err
-	}
-	return config, nil
-}
 func main() {
 	config, err := cnfg.NewConfigPath(ConfigPath)
 	if err != nil {
@@ -38,5 +27,5 @@ func main() {
 		_ = fmt.Errorf("%s", err.Error())
 		return
 	}
-	defer client.Connect.Conn.Close()
+
 }
